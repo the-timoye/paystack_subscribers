b55179c9f6afd93ef7bbff6cdd7b7bad22c2a2f1
the-timoye <thetimoye@gmail.com>
2021-11-08 16:49:51 +0100

    feat(API): setup requests setup

---
+++ api.py
@@ -0,0 +1,38 @@
+import requests
+import configparser
+
+config = configparser.ConfigParser();
+
+config.read('env.cfg')
+
+headers = {
+    "Authorization" : f"Bearer {config['API']['SECRET_KEY']}",
+    "Content-Type": "application/json"
+    } 
+
+def get_request(path, *args, **kwargs):
+    return requests.get(
+        f'{config["API"]["BASE_URL"]}{path}',
+         headers=headers, 
+         *args, 
+         **kwargs
+    );
+
+def post_request(path, data, *args, **kwargs):
+    return requests.post(
+        f'{config["API"]["BASE_URL"]}{path}',
+        headers=headers, 
+        data=data, 
+        *args, 
+        **kwargs
+    );
+
+
+if __name__ == '__main__':
+    try:
+        response = get_request('plan',)
+        print(response.json())
+    except requests.ConnectionError as httperr:
+        print('Invalid URL')
+    except Exception as e:
+        print(e)
